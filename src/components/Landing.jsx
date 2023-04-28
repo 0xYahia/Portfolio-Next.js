ffd1b9c069b1406e7467e4d8043f8d5d56f8feb1
yahia <mohamedyahia207545>
2023-04-28 16:16:25 +0300

    tech section

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -13,7 +13,13 @@ const Landing = () => {
       <Layout className="pt-0">
         <div className="flex items-center justify-between w-full">
           <div className="w-1/2">
-            <Image src={profilePic1} alt="Yahia" className="w-full h-auto" />
+            <Image
+              src={profilePic1}
+              alt="Yahia"
+              className="w-full h-auto"
+              priority
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+            />
           </div>
           <div className="w-1/2 flex flex-col items-center self-center">
             <AnimatedText
