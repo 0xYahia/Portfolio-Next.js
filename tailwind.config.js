295915a7aa857fe40485c326201857ac59372362
Yahia <mohamedyahia207545@gmail.com>
2023-07-24 23:42:21 +0300

    last update

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,8 +23,9 @@ module.exports = {
       },
     },
     screens: {
+      '3xl': { max: '1920px' },
+      // => @media(max-width: 1920px) {...}
       '2xl': { max: '1535px' },
-      // => @media(max-width: 1535px) {...}
       xl: { max: '1279px' },
       // => @media(max-width: 1279px) {...}
       lg: { max: '1023px' },
